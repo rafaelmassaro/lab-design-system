e912dbf9be9913018c12b5ce37d7d8a12efd17bf
Rafael <rafaelmassaro9@gmail.com>
2022-10-12 19:50:03 -0300

    First commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32
+    },
+    colors: {
+      gray: {
+        100: '#e1e1e6',
+        200: '#c4c4cc',
+        400: '#7c7cba',
+        800: '#202024',
+        900: '#121214',
+      },
+      cyan: {
+        300: '#9be1fb',
+        500: '#81d8f7',
+      },
+      
+      black: '#000',
+      white: '#fff',
+      transparent: 'transparent', 
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
